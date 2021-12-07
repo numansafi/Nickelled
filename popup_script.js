e70cefaba51ee7a9fb479ee4768b2a16dfecdb2b
Numan Safi <numansafi@live.com>
2021-12-07 20:36:39 +0000

    added google login to extension

--- popup_script.js
+++ popup_script.js
@@ -7,8 +7,41 @@ const firebaseConfig = {
   appId: "1:189347627734:web:3474b45ee3468c0757bf77",
 };
 
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // Initialize the FirebaseUI Widget using Firebase.
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+const uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      chrome.runtime.sendMessage({ message: "sign_in" }, function (response) {
+        if (response.message === "success") {
+          window.location.replace("./welcome.html");
+        }
+      });
+      return false;
+    },
+    uiShown: function () {
+      document.getElementById("my_sign_in").style.display = "none";
+      document.getElementById("wrapper").style.pointerEvents = "none";
+    },
+  },
+  signInFlow: "popup",
+  // signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    {
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      customParameters: {
+        prompt: "select_account",
+      },
+    },
+  ],
+  // Terms of service url.
+  // tosUrl: '<your-tos-url>',
+  // Privacy policy url.
+  // privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+ui.start("#sign_in_options", uiConfig);
