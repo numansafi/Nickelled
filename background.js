3f6e8a16c554aff2074e0d996316b887fface7cb
Numan Safi <numansafi@live.com>
2021-12-07 17:33:25 +0000

    functions for signin/signout have been added to background.js

--- background.js
+++ background.js
@@ -7,5 +7,13 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       message: "success",
       payload: user_signed_in,
     });
+  } else if (request.message === "sign_out") {
+    user_signed_in = false;
+    sendResponse({ message: "success" });
+  } else if (request.message === "sign_in") {
+    user_signed_in = true;
+    sendResponse({ message: "success" });
   }
+
+  return true;
 });
